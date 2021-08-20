aa415905a28ab30a099abc322d3639cf4878805c
Mohammed Rawther <mohammedabdulsalamrawther.b20me1130@mbcet.ac.in>
2021-08-20 23:44:28 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  let [num,setNum]=useState(0)
+  
+  const increment=()=>{
+   setNum(num+1)
+  }
+  
+  const decrement=()=>{
+   setNum(num-1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="main__container">
+        <h1 id="title">NEVER MISS YOUR COUNT AGAIN!!!</h1>
+        <h1 id="NUMBER">{num}</h1>
+        <div id="btn_collection">
+          <button onClick={increment}>+</button>
+          <button onClick={decrement}>-</button>
+        </div>
+      </div>
     </div>
   );
 }
